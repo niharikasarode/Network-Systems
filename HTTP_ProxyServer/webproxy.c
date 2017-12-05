b780dd516ea7e7a57e61af0c2a252c196d7441c5
niharikasarode <Nharika.Sarode@colorado.edu>
2017-12-05 06:32:48 +0530

    added function to prevent DNS Query

--- HTTP_ProxyServer/webproxy.c
+++ HTTP_ProxyServer/webproxy.c
@@ -24,10 +24,10 @@ char buf[MAXLINE];
 struct sockaddr_in cliaddr, servaddr, mainserver_addr;
 char recv_buff[buff_max_size], req_method[10], URL_slash[1024], http_version[10], *url, site[1024], *path;
 char send_request[buff_max_size], rec_buf[buff_max_size], *temp, send_buff[buff_max_size];
-char cache_dir[2048], Full_md5[100], dir[100];
+char cache_dir[2048], Full_md5[100], dir[100], IP[100];
 char method_error[2048], *conf_buffer, fname[100];
 struct hostent *hp;
-FILE *fp, *f1, *f2;
+FILE *fp, *f1, *f2, *f3;
 time_t time_now;
 
 void calculate_md5(char *string)
@@ -61,9 +61,7 @@ int check_md5(char *hash_str, int timeout)
         bzero(dir, sizeof(dir));
         strncpy(dir, cache_dir, strlen(cache_dir));
         strcat(dir,"md5list");
-        printf("File path for list of stored md5: ");
-        puts(dir);
-
+        
         f1 = fopen(dir, "rb");        
         if(f1 != NULL)
         {
@@ -95,6 +93,43 @@ int check_md5(char *hash_str, int timeout)
                 return 1;
         }
 }
+
+
+int check_hostname(char *hostname, char *c_dir)
+{
+
+        bzero(dir, sizeof(dir));
+        size_t max = 200;
+        strncpy(dir, c_dir, strlen(c_dir));
+        strncat(dir, "host_list", strlen("host_list"));
+        conf_buffer = (char *)malloc(max*sizeof(char));
+        
+        char *tok;
+        int u = 3;
+        f3 = fopen(dir, "rb");
+
+        if(f3 != NULL)
+        {
+                        getline(&conf_buffer,&max,f3);
+                        if(strncmp(conf_buffer, hostname, strlen(hostname)) == 0)
+                        {
+                                bzero(conf_buffer, sizeof(conf_buffer));
+                                getline(&conf_buffer,&max,f3);
+                                tok = strtok(conf_buffer, "\n");
+                                puts(tok);                                
+                                bzero(IP, sizeof(IP));
+                                strncpy(IP, tok, strlen(tok));
+                                puts(IP);
+                                u=2;
+                        }
+        fclose(f3);
+        }
+        free(conf_buffer);
+        return u;
+}
+
+
+
 
 void handle_request(int socketfd, char *cache_directory, int timeout)
 {
@@ -215,24 +250,56 @@ void handle_request(int socketfd, char *cache_directory, int timeout)
                                 bzero(bufer, sizeof(bufer));
                                 fclose(f1);
                         }
-                        
-                        hp = gethostbyname(site);
-                        if(hp == NULL)
-                        {
-                                printf("Host :");
-                                puts(site);
-                                printf("ERROR: gethostbyname failed\n");
-                                exit(1);
+
+
+                         /**  Check if site was already accessed to avoid extra DNS query**/
+                                        /* Returns 2 on success and 3 on fail*/
+                        int st;
+                        st = check_hostname(site, cache_directory);
+
+                        if(st == 3)
+                        {  
+                                printf("Need to do DNS query\n");                      
+                                hp = gethostbyname(site);
+                                if(hp == NULL)
+                                {
+                                        printf("Host :");
+                                        puts(site);
+                                        printf("ERROR: gethostbyname failed\n");
+                                        exit(1);
+                                }
+
+                                printf("actual name - %s\n", hp->h_name);
+                                printf("address - %s\n", inet_ntoa(*((struct in_addr *)hp->h_addr)));
+
+                                /* Save hostname n IP for future reference*/
+
+                                f3 = fopen(dir, "a");
+                                char bufer[100];
+                                if(f3 != NULL)
+                                {
+                                        fwrite(site, 1, strlen(site), f3);
+                                        fwrite("\n", 1, strlen("\n"), f3);
+                                        sprintf(bufer, "%s\n", inet_ntoa(*((struct in_addr *)hp->h_addr)));
+                                        fwrite(bufer, 1, strlen(bufer), f3);
+                                        bzero(bufer, sizeof(bufer));
+                                        fclose(f3);       
+                                }
                         }
+
                         bzero((char*)&mainserver_addr, sizeof(mainserver_addr));
 
-                        printf("actual name - %s\n", hp->h_name);
-                        printf("address - %s\n", inet_ntoa(*((struct in_addr *)hp->h_addr)));
-
-
                         mainserver_addr.sin_family = AF_INET;
-                        mainserver_addr.sin_port = htons(80);
-                        bcopy((char*)hp->h_addr, (char*)&mainserver_addr.sin_addr.s_addr,hp->h_length );
+                        mainserver_addr.sin_port = htons(port);
+                        if(st == 3)
+                        {
+                                bcopy((char*)hp->h_addr, (char*)&mainserver_addr.sin_addr.s_addr,hp->h_length );
+                        }
+                        else
+                        {
+                                 printf("No need of DNS query\n");
+                                 bcopy(IP, (char*)&mainserver_addr.sin_addr.s_addr, strlen(IP));
+                        }
 
                         if ((site_sock = socket (AF_INET, SOCK_STREAM, 0)) <0) 
                         {
@@ -247,7 +314,7 @@ void handle_request(int socketfd, char *cache_directory, int timeout)
                         exit(1);
                         }
 
-                        printf("Connected to %s on IP : %s\n\n", url, inet_ntoa(*((struct in_addr *)hp->h_addr)));
+                        //printf("Connected to %s on IP : %s\n\n", site, inet_ntoa(*((struct in_addr *)hp->h_addr)));
 
                         if(strlen(path) != 0)
                         sprintf(send_request, "GET %s %s\r\nHost: %s\r\nConnection: keep-alive\r\n\r\n", path, http_version, site);
@@ -292,7 +359,7 @@ void handle_request(int socketfd, char *cache_directory, int timeout)
                 else if(cache_present == 5)
                 {
 
-                        printf("\n Valid cacned copy exists\n\n");
+                        printf("\n*****  Valid cached copy exists  ******\n\n");
                         sprintf(fname, "./cache/%s.html",Full_md5);
                         fp = fopen(fname, "rb");
                         int n,d,fsize=0, size_flag=0;
